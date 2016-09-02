2dd8b79be36223c6d4730799ef0842da191553fa
Matthew Wallace <matthewwallace1991@icloud.com>
2016-09-02 16:30:28 +0100

    Offset background

---
+++ New Unity Project/Assets/Scroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroller : MonoBehaviour {
+
+	public float scrollSpeed = 0;
+//	private Vector2 savedOffset;
+	private Renderer rend;
+
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+
+	void Update () {
+		float offset = Time.time * scrollSpeed;
+		rend.material.mainTextureOffset = new Vector2 (offset, 0);
+	}
+}
